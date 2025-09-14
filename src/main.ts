c88d7bd939a43caa73320b62c4d2457a59e85ebf
hxh <3148182735@qq.com>
2025-09-14 18:54:22 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+
+
+
+
+createApp(App)
+.use(store)
+.use(router)
+.use(VueAxios,axios)
+.use(ElementPlus , {
+    locale: zhCn,
+})
+.use(createPinia())
+.mount('#app')
